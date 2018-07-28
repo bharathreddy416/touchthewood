6ea907a7aaa72a6fddebf38b6e774f917532acdc
Bharath Reddy Bollampally <bharath.bollampally@techaspect.com>
2018-07-28 17:50:46 +0530

    updated libraries

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <title>Touch The Wood</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/3.8.0/less.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   </head>
   <body>
 
