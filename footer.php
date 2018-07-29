5fbf11c8f4aa7473e0e597637a03ccc2813a4120
Varun Kumar Tuniki <varunkumar.tuniki@techaspect.com>
2018-07-29 18:55:00 +0530

    header and footer UI changes

--- footer.php
+++ footer.php
@@ -1,34 +1,37 @@
-<footer>
-  <?php
-    $args = array(
-      'theme_location' => 'header-menu',
-      'container' => 'nav',
-    );
-    wp_nav_menu( $args );
-  ?>
-
-  <div class="location">
-    <p>Address: </p>
-    <p>Phone number: </p>
-  </div>
-
-  <div class="socials">
-    <?php
+  <footer>
+    <div class="container-fluid">
+      <?php
       $args = array(
-        'theme_location' => 'social-menu',
+        'theme_location' => 'header-menu',
         'container' => 'nav',
-        'container_class' => 'socials',
-        'container_id' => 'socials',
-        'link_before' => '<span class="sr-text">',
-        'link_after' => '</span>'
-      );
+        'container_class' => 'footer-menu col-xs-6 col-sm-6'
+        );
       wp_nav_menu( $args );
-    ?>
-  </div>
+      ?>
 
-  <p class="copyright">All rights reserved <?php echo date('Y'); ?> </p>
+      <div class="location col-xs-6 col-sm-6">
+        <p>Address: </p>
+        <p>Phone number: </p>
+      </div>
+
+      <div class="socials">
+        <?php
+        $args = array(
+          'theme_location' => 'social-menu',
+          'container' => 'nav',
+          'container_class' => 'socials',
+          'container_id' => 'socials',
+          'link_before' => '<span class="sr-text">',
+          'link_after' => '</span>'
+          );
+        wp_nav_menu( $args );
+        ?>
+      </div>
+
+      <p class="copyright col-xs-12">© All rights reserved <?php echo date('Y'); ?> </p>
+    </div>
   </footer>
 
   <?php wp_footer(); ?>
- </body>
+</body>
 </html>
--- header.php
+++ header.php
@@ -1,35 +1,40 @@
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Touch The Wood</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
-	  <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow" rel="stylesheet">
-  </head>
-  <body class="custom-style">
+<head>
+	<meta charset="utf-8">
+	<title>Touch The Wood</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php wp_head(); ?>
+	<link href="https://fonts.googleapis.com/css?family=Archivo+Narrow" rel="stylesheet">
+</head>
+<body class="custom-style">
 
-  <header class="site-header">
-    <div class="main-header">
-		<div class="container">
-			<div class="row">
-			  <div class="logo col-xs-3 col-md-3">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-				  <img src="<?php echo get_template_directory_uri() ?>/images/logo.png">
-				</a>
-			  </div>
-			  <div class="main-menu col-xs-12 col-md-9">
-				<div class="navigation pull-right">
-				  <?php
+	<header class="site-header">
+		<div class="main-header">
+			<div class="main-menu">
+				<nav class="navbar navbar-inverse container-fluid site-nav">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>                        
+						</button>
+						<div class="logo">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+								<img src="<?php echo get_template_directory_uri() ?>/images/logo.png">
+							</a>
+						</div>
+					</div>
+					<?php
 					$args= array(
-					  'theme_locations' => 'header-menu',
-					  'container' => 'nav',
-					  'container_class' => 'site-nav',
-					);
+						'theme_locations' => 'header-menu',
+						'container' => 'div',
+						'menu_class' => 'nav navbar-nav',
+						'container_class' => 'collapse navbar-collapse nav-menu',
+						'container_id' => 'myNavbar'
+						);
 					wp_nav_menu( $args );
-				  ?>
-				</div>
-			  </div>
-			 </div>
+					?>
+				</nav>
+			</div>
 		</div>
-    </div>
-  </header>
+	</header>
