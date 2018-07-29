64a3f9467730b7762826752f9d03fa9f91f0b72c
Bharath Reddy Bollampally <bharath.bollampally@techaspect.com>
2018-07-29 21:22:21 +0530

    css and contact us changes

--- page-contact-us.php
+++ page-contact-us.php
@@ -1,36 +1,41 @@
 <?php get_header(); ?>
 
     <?php while(have_posts()): the_post(); ?>
-
       <div class="contact-us" style="background-image:url(<?php echo get_the_post_thumbnail_url(); ?>)">
-        <div class="contact-us-text">
-          <h2><?php the_title(); ?></h2>
-        </div>
+		  <div class="container">
+			<div class="contact-us-text">
+			  <h2><?php the_title(); ?></h2>
+			</div>
+		  </div>
       </div>
 
       <div class="contact-info">
-          <form class="contact-form" method="post" action="">
-            <div class="field">
-              <input type="text" name="name" placeholder="Name" required />
-            </div>
-            <div class="field">
-              <input type="email" name="email" placeholder="E-Mail" required />
-            </div>
-            <div class="field">
-              <input type="tel" name="phone" placeholder="Phone" required />
-            </div>
-            <div class="field">
-              <input type="text" name="city" placeholder="Your City" required />
-            </div>
-            <div class="field">
-              <textarea name="message" placeholder="Message" requires></textarea>
-            </div>
-            <input type="submit" name="submit-contact" class="button" value="Send" />
-
-            <input type="hidden" name="hidden" value="1" />
-
-          </form>
+		  <div class="container">
+			  <div class="row">
+					<form class="contact-form col-xs-12 col-sm-6 col-md-5" method="post" action="">
+						<div class="field">
+						  <input type="text" name="name" placeholder="Name" required />
+						</div>
+						<div class="field">
+						  <input type="email" name="email" placeholder="E-Mail" required />
+						</div>
+						<div class="field">
+						  <input type="tel" name="phone" placeholder="Phone" required />
+						</div>
+						<div class="field">
+						  <input type="text" name="city" placeholder="Your City" required />
+						</div>
+						<div class="field">
+						  <textarea name="message" placeholder="Message" requires></textarea>
+						</div>
+						<div class="field">
+							<input type="submit" name="submit-contact" class="button" value="Send" />
+							<input type="hidden" name="hidden" value="1" />
+						</div>
+					</form>
+			  </div>
         </div>
+	</div>
 
     <?php endwhile; ?>
 
