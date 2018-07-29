89131e05d4541fcfab7339c47820f4ab9cb662db
Bharath Reddy Bollampally <bharath.bollampally@techaspect.com>
2018-07-29 13:29:17 +0530

    UI changes related to header and grid components

--- footer.php
+++ footer.php
@@ -30,10 +30,8 @@
   </footer>
 
   <?php wp_footer(); ?>
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="wp-content/themes/touchthewood/js/bootstrap.js" type="text/javascript"></script>
   
  </body>
 </html>
--- header.php
+++ header.php
@@ -4,28 +4,33 @@
     <title>Touch The Wood</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="wp-content/themes/touchthewood/css/bootstap.css">
+	  <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow" rel="stylesheet">
   </head>
-  <body>
+  <body class="custom-style">
 
   <header class="site-header">
     <div class="main-header">
-      <div class="logo">
-        <a href="#<?php echo esc_url( home_url( '/' ) ); ?> ">
-          <img src="<?php echo get_template_directory_uri() ?>/images/logo.png">
-        </a>
-      </div>
-      <div class="main-menu">
-        <div class="navigation">
-          <?php
-            $args= array(
-              'theme_locations' => 'header-menu',
-              'container' => 'nav',
-              'container_class' => 'site-nav',
-            );
-            wp_nav_menu( $args );
-          ?>
-        </div>
-      </div>
+		<div class="container">
+			<div class="row">
+			  <div class="logo col-xs-3 col-md-3">
+				<a href="#<?php echo esc_url( home_url( '/' ) ); ?> ">
+				  <img src="<?php echo get_template_directory_uri() ?>/images/logo.png">
+				</a>
+			  </div>
+			  <div class="main-menu col-xs-12 col-md-9">
+				<div class="navigation pull-right">
+				  <?php
+					$args= array(
+					  'theme_locations' => 'header-menu',
+					  'container' => 'nav',
+					  'container_class' => 'site-nav',
+					);
+					wp_nav_menu( $args );
+				  ?>
+				</div>
+			  </div>
+			 </div>
+		</div>
     </div>
   </header>
